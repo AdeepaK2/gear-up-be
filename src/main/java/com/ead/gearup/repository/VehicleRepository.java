9e4d766790e7415d72273f05cb2665366988af38
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-11-05 11:08:26 +0530

    chore: remove unused package

--- src/main/java/com/ead/gearup/repository/VehicleRepository.java
+++ src/main/java/com/ead/gearup/repository/VehicleRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ead.gearup.model.Customer;
 import com.ead.gearup.model.Vehicle;
 
 @Repository
